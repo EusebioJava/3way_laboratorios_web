dacc3f12dda737b16582ea4a76df83de9c34a151
eulim <eulim@DESKTOP-UP7TBUO>
2017-12-22 06:56:34 -0200

    Alteração 4

--- Livraria3Way/src/model/Livro.java
+++ Livraria3Way/src/model/Livro.java
@@ -16,5 +16,42 @@ public class Livro {
 	private String imagem;
 	private String descricao;
 	//getter e setters omitidos
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public String getImagem() {
+		return imagem;
+	}
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
 	
 }
