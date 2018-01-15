ba0db24f6a877ed1260ab19d6d0cd8cd7259f45d
eulim <eulim@DESKTOP-UP7TBUO>
2018-01-15 00:35:36 -0200

    Altera

--- Livraria3Way/src/br/com/triway/dao/Pesquisa.java
+++ Livraria3Way/src/br/com/triway/dao/Pesquisa.java
@@ -1,27 +1,34 @@
 package br.com.triway.dao;
 
+import java.util.List;
+
 import service.Livro;
 
 public class Pesquisa {
 
-	public static void main(String[] args) {
+	public static List<Livro> main(String[] args, List<Livro> livros) {
 		String titulo = "CASSINO";
-		LivroDao dao = new LivroDao();
-		dao.consultar(titulo).forEach(p -> System.out.println("Desc: " + p.getDescricao() + " Preço:" + p.getPreco()));
-		//if(titulo == null){
-	//		titulo = "";
-	//		} 
-	//		System.out.println("Pesquisa: "+ titulo);
-		//	LivroDao dao = new LivroDao();
-		//	livros = dao.consultar(titulo);
-		//	if(livros.size() <= 0){
-		//	System.out.println("Livro não localizado");
-	//	} return livros;
+		LivroDao daoo = new LivroDao();
+		daoo.consultar(titulo)
+		.forEach(
+				p -> System.out.println("Desc: " + p.getDescricao() + " Preço:" + p.getPreco())
+				);
+		if(titulo == null){
+			titulo = "";
+		}
+		
+			System.out.println("Pesquisa: "+ titulo);
+			LivroDao dao1 = new LivroDao();
+			livros = dao1.consultar(titulo);
+		if(livros.size() <= 0){
+			System.out.println("Livro não localizado");
+		} 
+			return livros;}
 
-	//public Livro verLivro(Integer codigo){
-		//LivroDao dao = new LivroDao();
-	//	livro = dao.consultar(integer codigo);
-	//	return livro;
+	public Livro verLivro(Integer codigo){
+		LivroDao dao2 = new LivroDao();
+		Livro livros2 = dao2.consultar(codigo);
+		return livros2;
 		//OBS: dao.consultar(titulo) e dao.consultar(codigo) são métodos do LivroDao, 
 		//mas que buscam atributos diferentes
 		//(um busca uma lista de livros e outro busca somente um livro).
